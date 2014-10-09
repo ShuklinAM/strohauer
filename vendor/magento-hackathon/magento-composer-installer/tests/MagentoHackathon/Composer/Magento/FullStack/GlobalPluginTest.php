b9b8d25b84000b186296459861907ad11aff7974
ShuklinAM <screamzt@gmail.com>
2014-10-09 18:19:13 +0300

    upload project

---
+++ vendor/magento-hackathon/magento-composer-installer/tests/MagentoHackathon/Composer/Magento/FullStack/GlobalPluginTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MagentoHackathon\Composer\Magento\FullStack;
+
+use Composer\Util\Filesystem;
+use Symfony\Component\Process\Process;
+
+class GlobalPluginTest extends AbstractTest
+{
+    
+    protected static $processLogCounter = 1;
+
+    protected function setUp()
+    {
+        
+    }
+    
+    protected function tearDown()
+    {
+        
+    }
+
+    protected function prepareCleanDirectories()
+    {
+        $fs = new Filesystem();
+        $fs->removeDirectory( self::getBasePath().'/home/vendor' );
+        $fs->removeDirectory( self::getBasePath().'/home/cache' );
+        $fs->remove(          self::getBasePath().'/home/composer.lock' );
+    }
+
+    public function testGlobalInstall()
+    {
+        $process = new Process(
+            self::getComposerCommand().' global install',
+            self::getProjectRoot()
+        );
+        $process->setEnv( array('COMPOSER_HOME'=>self::getBasePath().'/home'));
+
+        $process->run();
+        $this->assertProcess($process);
+    }
+    
+    public function testGlobalUpdate()
+    {
+
+        $process = new Process(
+            self::getComposerCommand().' global update',
+            self::getProjectRoot()
+        );
+        $process->setEnv( array('COMPOSER_HOME'=>self::getBasePath().'/home'));
+
+        $process->run();
+        $this->assertProcess($process);
+    }
+
+}
